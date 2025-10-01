6c7e74b1f9c461411eae178b0f20459972ae3434
MatteoGabriele <m.gabriele.dev@gmail.com>
2025-10-01 08:58:33 +0200

    fix: height jump on loading

--- app/app.vue
+++ app/app.vue
@@ -63,7 +63,9 @@ async function handleRetry() {
 <template>
   <NuxtLayout>
     <Confetti v-if="isAnswerCorrect" />
-    <div class="flex w-full max-w-4xl min-h-[600px] justify-center">
+    <div
+      class="flex w-full max-w-4xl min-h-[600px] 2xl:min-h-[720px] justify-center"
+    >
       <div
         class="flex items-center justify-center gap-2 text-neutral-500"
         v-if="pendingGifs || pendingRepos"
