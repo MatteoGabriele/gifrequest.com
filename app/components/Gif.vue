3e6b6031ffda4b65f20cf783a3645fa0fe68ac5b
MatteoGabriele <m.gabriele.dev@gmail.com>
2025-10-02 21:59:03 +0200

    refactor: improve loeader

--- app/components/Gif.vue
+++ app/components/Gif.vue
@@ -16,10 +16,19 @@ function handleLoaded() {
 <template>
   <div class="relative flex items-center justify-center aspect-3/2">
     <img
+      v-if="isPending"
       class="absolute w-full h-full object-cover"
       src="https://media1.tenor.com/m/kbs-kzp0DYUAAAAC/static-tv.gif"
       alt="static tv effect"
     />
+
+    <PhSpinner
+      v-if="isPending"
+      :size="48"
+      weight="bold"
+      class="animate-spin z-10 bottom-4 right-4 absolute text-white"
+    />
+
     <img
       @load="handleLoaded"
       :class="[
