e10dfa58bc98165f41c83a0a7f359f0badf6fedc
MatteoGabriele <m.gabriele.dev@gmail.com>
2025-10-28 19:25:18 +0100

    fix: avoid rendering image without source

--- app/components/Gif.client.vue
+++ app/components/Gif.client.vue
@@ -20,12 +20,13 @@ function handleLoaded() {
     class="relative shadow-sm w-full aspect-video overflow-hidden rounded-4xl"
   >
     <ProgressiveImage
+      v-if="src"
       @success="handleLoaded"
       object-cover
       class="absolute inset-0"
       placeholder-src="https://media1.tenor.com/m/kbs-kzp0DYUAAAAC/static-tv.gif"
       fallback-src="https://media1.tenor.com/m/51xvC35-fDEAAAAd/manhunt.gif"
-      :src="src ?? ''"
+      :src="src"
     />
   </div>
 </template>
